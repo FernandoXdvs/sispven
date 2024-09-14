2f0db8526bd74462bd0e3e84251b83548042f2fc
Fernando Jp <fernandojoaquinpereyda@gmail.com>
2024-09-14 10:27:29 -0600

    Agregando header, menu, contenido principal y footer

---
+++ vistas/modulos/header.php
@@ -0,0 +1,64 @@
+<!--Barra de navegación-->
+<nav class="main-header navbar navbar-expand navbar-green navbar-dark">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <!--Botón de navegación-->
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!--Perfil usuario-->
+        <li class="nav-item">
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                            <img src="vistas/img/usuarios/default/anonymous.png" class="user-image"
+                                style="width: 25px; height:25px;">
+                            <span class="hidden-xs">Usuario Administrador</span>
+                        </a>
+                        <!--Dropdown-toggle -->
+                        <ul class="dropdown-menu">
+                            <li class="user-body">
+                                <div class="pull-right">
+                                    <a href="#" class="btn btn-default btn-flat">Salir</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <!-- Navbar Search -->
+        <li class="nav-item">
+            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                <i class="fas fa-search"></i>
+            </a>
+            <div class="navbar-search-block">
+                <form class="form-inline">
+                    <div class="input-group input-group-sm">
+                        <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                            aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-navbar" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </li>
+        <!--Fullscreen-->
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+    </ul>
+
+</nav>
---
+++ vistas/modulos/menu.php
@@ -0,0 +1,103 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-alert elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+        <img src="vistas/img/plantilla/minlogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-bold">SISPVEN WEB</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="vistas/img/usuarios/default/anonymous.png" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Usuario Administrador</a>
+            </div>
+        </div>
+    </div>
+    <!-- Sidebar Menu -->
+    <nav>
+        <ul class="nav nav-pills nav-sidebar flex-column " data-widget="treeview" role="menu" data-accordion="false">
+            <!--Home-->
+            <li class="nav-item">
+                <a href="#" class="nav-link active mx-auto">
+                    <i class="nav-icon fa fa-home"></i>
+                    <p>
+                        Inicio
+                    </p>
+                </a>
+            </li>
+            <!--Gestor de usuarios-->
+            <li class="nav-item">
+                <a href="#" class="nav-link  mx-auto">
+                    <i class="nav-icon fa fa-user"></i>
+                    <p>
+                        Usuarios
+                    </p>
+                </a>
+            </li>
+            <!--Categorias-->
+            <li class="nav-item">
+                <a href="#" class="nav-link  mx-auto">
+                    <i class="nav-icon fa fa-th"></i>
+                    <p>
+                        Categorías
+                    </p>
+                </a>
+            </li>
+            <!--Productos-->
+            <li class="nav-item">
+                <a href="#" class="nav-link  mx-auto">
+                    <i class="nav-icon fa fa-store"></i>
+                    <p>
+                        Productos
+                    </p>
+                </a>
+            </li>
+            <!--Clientes-->
+            <li class="nav-item">
+                <a href="#" class="nav-link  mx-auto">
+                    <i class="nav-icon fa fa-users"></i>
+                    <p>
+                        Clientes
+                    </p>
+                </a>
+            </li>
+            <!--Ventas-->
+            <li class="nav-item">
+                <a href="#" class="nav-link  mx-auto">
+                    <i class="nav-icon fa fa-list-ul"></i>
+                    <p>
+                        Ventas
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Administrar Ventas</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Crear Venta</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Reporte de ventas</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+        </ul>
+    </nav>
+
+</aside>
